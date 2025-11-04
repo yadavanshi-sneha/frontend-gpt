0f0dd6a1edd60720e102c93068a660ff0c52154a
Sneha kumari <snehakumari76373@gmail.com>
2025-11-04 08:50:58 +0530

    Add files

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css';
+import Sidebar from "./Sidebar.jsx";
+import ChatWindow from "./ChatWindow.jsx";
+import { MyContext } from "./MyContext.jsx";
+import {useState} from 'react';
+import {v1 as uuidv1} from "uuid";
+
+function App() {
+   const [prompt, setPrompt] = useState("");
+   const [reply , setReply] = useState(null);
+   const [currThreadId, setCurrThreadId] = useState(uuidv1());
+   const [prevChats, setPrevChats] = useState([]); //array of objects {threadId, lastMessage} , stores all chats of curr threads
+   const [newChat, setNewChat] = useState(true); //to create new chat when clicked on new chat button in sidebar
+   const [allThreads, setAllThreads] = useState([]); //array of objects {threadId, lastMessage} , stores all chats of all threads
+   
+   const providerValues = {
+      prompt,setPrompt,
+      reply, setReply,
+      currThreadId,setCurrThreadId,
+      newChat,setNewChat,
+      prevChats,setPrevChats,
+      allThreads,setAllThreads
+
+   }; //passing values
+
+  return (
+     <div className='app'>
+         <MyContext.Provider value={providerValues}>
+          <Sidebar></Sidebar>
+          <ChatWindow></ChatWindow>
+          </MyContext.Provider>
+     </div>
+  )
+}
+export default App;
---
+++ src/ChatWindow.jsx
@@ -0,0 +1,105 @@
+import { useContext,useState,useEffect} from 'react';
+import "./ChatWindow.css"
+import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import {ScaleLoader} from "react-spinners";
+
+
+function ChatWindow(){
+    const {prompt, setPrompt, reply,setReply,currThreadId, prevChats, setPrevChats, setNewChat} = useContext(MyContext);
+    const [loading,setLoading] = useState(false);
+    const [ isOpen, setIsOpen] = useState(false); // set default false value
+
+    const getReply = async() => {
+          setLoading(true);
+          setNewChat(false);
+        console.log("message", prompt, "threadId", currThreadId);
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        };
+
+        try{
+             const response = await fetch("http://localhost:8000/api/chat",options);
+             const res = await response.json();
+             console.log(res);
+             setReply(res.reply);
+            } catch(err){
+            console.log(err);
+        }
+        setLoading(false);
+
+    }
+
+
+    // Append new Chat to prevChats
+    useEffect(() => {
+          if(prompt&&reply) {
+            setPrevChats(prevChats => (
+                [...prevChats, {
+                    role: "user",
+                    content: prompt
+                }, {
+                    role: "assistant",
+                    content:reply
+            
+                }]
+            ));
+          }
+
+          setPrompt("");
+    }, [reply]);
+
+
+    const  handleProfileClick = () => {
+           setIsOpen(!isOpen);
+    }
+
+
+    return (
+    <div className="chatWindow">
+         <div className="navbar">
+         <span>AapnaGPT <i className="fa-solid fa-chevron-down"></i></span>
+         <div className="usericonDiv" onClick={handleProfileClick}>
+         <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+         </div>
+         </div>
+        
+        {
+            isOpen &&
+            <div className="dropDown">
+            <div className="dropDownItem"><i class="fa-solid fa-cloud-arrow-up"></i>Upgrade plan</div>
+            <div className="dropDownItem"><i class="fa-solid fa-gear"></i>Settings</div>
+            <div className="dropDownItem"><i class="fa-solid fa-right-from-bracket"></i>Log out</div>
+            </div>
+        }
+         <Chat></Chat>
+         <ScaleLoader  color="#fff" loading={loading}>
+
+         </ScaleLoader>
+
+         <div className="chatInput">
+            <div className="inputBox">
+                <input placeholder="Ask anything"
+                       value={prompt}
+                       onChange={(e) => setPrompt(e.target.value)}
+                       onKeyDown={(e) => e.key === 'Enter'? getReply(): ''}
+                       >
+                </input>
+                <div id="submit" onClick={getReply}><i className="fa-solid fa-paper-plane"></i> </div>
+            </div>
+            <p className="info">
+                SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+            </p>
+         </div>
+    </div>
+    )
+
+}
+export default ChatWindow;
